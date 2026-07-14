0a68dd6e2a2e39e305173a454922b9a06b18cd94
Bijoy Barman <bijoy20-127@aec.ac.in>
2026-07-14 23:17:17 +0530

    #react4

---
+++ React/React4/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./App.css";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-white bg-black p-12'>hello</h1>
+    </div>
+  )
+}
+
+export default App
