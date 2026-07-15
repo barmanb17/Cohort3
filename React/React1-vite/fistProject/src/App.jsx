c8f3e62d0863bfc572d64cdbbcd4ae53a86a3ad3
Bijoy Barman <bijoy20-127@aec.ac.in>
2026-07-15 21:50:35 +0530

    #level4

--- React/React1-vite/fistProject/src/App.jsx
+++ React/React1-vite/fistProject/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const App = () => {
+  const employee = {
+  name: "Bijoy",
+  company: {
+    name: "Google",
+    location: {
+      city: "Bangalore",
+      state: "Karnataka",
+      pincode: 560001,
+    },
+  },
+};
   return (
-    <div>
-      <Employee
-      name="Bijoy"
-      />
-    </div>
+    <Employee employee={employee} />
   )
 }
 
--- React/React1-vite/fistProject/src/Employee.jsx
+++ React/React1-vite/fistProject/src/Employee.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Employee = ({name, experience = "Fresher"}) => {
+const Employee = (props) => {
   return (
     <div>
-        <h3>Name: {name} </h3>
-        <h3>Experience {experience} years</h3>
+        <h3>Name: {props.employee.name}</h3>
+        <h3>Company: {props.employee.company.name}</h3>
+        <h3>City: {props.employee.company.location.city}</h3>
+        <h3>State: {props.employee.company.location.state}</h3>
+        <h3>Pincode: {props.employee.company.location.pincode}</h3>
     </div>
   )
 }
