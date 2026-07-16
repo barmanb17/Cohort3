618ceae4a616103bb9e04a89d522b9b4381b3ed7
Bijoy Barman <bijoy20-127@aec.ac.in>
2026-07-16 15:49:28 +0530

    #level7

--- React/React1-vite/fistProject/src/App.jsx
+++ React/React1-vite/fistProject/src/App.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import Counter from './Counter'
-import Fruits from './Fruits'
-import User from './User'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [formData, setFormdata] = useState({
+    name: "",
+    email: ""
+  })
   return (
     <div>
-      <Fruits/>
-      <User/>
+      <input 
+      type="text" 
+      value={formData.name}
+      placeholder='Enter Your Name'
+      onChange={(e)=> setFormdata({
+        ...formData, name: e.target.value
+      })}
+
+      />
+
+      <input
+       type="email"
+       value={formData.email}
+       placeholder='Enter Your Email'
+       onChange={(e) => setFormdata({
+        ...formData, email: e.target.value
+       })}
+        />
+
+
+        <h2>Name: {formData.name}</h2>
+        <h2>Email: {formData.email}</h2>
     </div>
   )
 }
